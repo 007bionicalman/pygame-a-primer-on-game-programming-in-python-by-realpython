badd04356f358568854f20a8ce474473dc323256
007bionicalman <86726927+007bionicalman@users.noreply.github.com>
2022-10-12 13:37:51 -0700

    still cant get caption to change from the default

--- pygame_a_primer_on_game_programming_in_python_by_realpython.py
+++ pygame_a_primer_on_game_programming_in_python_by_realpython.py
@@ -1,3 +1,45 @@
+# Import the pygame module
+import pygame
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+pygame.init()
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+# Define a player object by extending pygame.sprite.Sprite
+# The surface drawn on the screen is now an attribute of 'player'
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+# Initialize pygame
+pygame.init()
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Instantiate player. Right now, this is just a rectangle.
+player = Player()
+
+# Variable to keep the main loop running
+running = True
+
+# Main loop
 #Import the pygame module.
 import pygame
 
@@ -19,7 +61,6 @@ pygame.init()
 #Define the constants for the screen width and height.
 SCREEN_WIDTH=800
 SCREEN_HEIGHT=600
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("First Sprite Player Ever")
 
@@ -60,11 +101,11 @@ while running:
         elif event.type==QUIT
             running=True
         
-            #Fill the screen black.
+                     #Fill the screen black.
     screen.fill((0, 0, 0))
     
 #Draw the Player on the screen
     screen.blit(player.surf,((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
         
                 #Update the display
-    pygame.display.flip() 
+    pygame.display.flip()
