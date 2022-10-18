fb03df1a9caa4962bae858626af9f28222275d43
007bionicalman <86726927+007bionicalman@users.noreply.github.com>
2022-10-18 07:26:27 -0700

    cant get player to move

--- pygame_a_primer_on_game_programming_in_python_by_realpython.py
+++ pygame_a_primer_on_game_programming_in_python_by_realpython.py
@@ -52,22 +52,17 @@ while running:
         # Check for QUIT event. If QUIT, then set running to false.
         elif event.type == QUIT:
             running = False
-
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -5)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+            if pressed_keys[K_DOWN]:
+                self.rect.move_ip(0, 5)
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
 
-        pressed_keys = pygame.key.get_pressed()
-    # Move the sprite based on user keypresses
-
-
-
+    pressed_keys = pygame.key.get_pressed()
 
     screen.fill((0, 0, 0))
 
